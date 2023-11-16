a53125c25e2ebc317d39c4c3e7ec0f99c971fbc5
Lakshminali20000 <143200984+Lakshminali20000@users.noreply.github.com>
2023-11-15 21:06:27 -0600

    update

--- controllers/fruit.js
+++ controllers/fruit.js
@@ -13,19 +13,41 @@ res.send(`{"error": ${err}}`);
 }
 };
 
-// for a specific fruit.
-exports.fruit_detail = function(req, res) {
-res.send('NOT IMPLEMENTED: fruit detail: ' + req.params.id);
-};
+exports.fruit_detail = async function(req, res) {
+    console.log("detail" + req.params.id)
+    try {
+    result = await fruit.findById( req.params.id)
+    res.send(result)
+    } catch (error) {
+    res.status(500)
+    res.send(`{"error": document for id ${req.params.id} not found`);
+    }
+    };
+    
 
 // Handle fruit delete form on DELETE.
 exports.fruit_delete = function(req, res) {
 res.send('NOT IMPLEMENTED: fruit delete DELETE ' + req.params.id);
 };
-// Handle fruit update form on PUT.
-exports.fruit_update_put = function(req, res) {
-res.send('NOT IMPLEMENTED: fruit update PUT' + req.params.id);
-};
+exports.fruit_update_put = async function(req, res) {
+    console.log(`update on id ${req.params.id} with body
+    ${JSON.stringify(req.body)}`)
+    try {
+    let toUpdate = await fruit.findById( req.params.id)
+    // Do updates of properties
+    if(req.body.NAME)
+    toUpdate.NAME = req.body.NAME;
+    if(req.body.COLOR) toUpdate.COLOR = req.body.COLOR;
+    if(req.body.CALORIES) toUpdate.CALORIES = req.body.CALORIES;
+    let result = await toUpdate.save();
+    console.log("Sucess " + result)
+    res.send(result)
+    } catch (err) {
+    res.status(500)
+    res.send(`{"error": ${err}: Update for id ${req.params.id}
+    failed`);
+    }
+    };
 
 // VIEWS
 // Handle a show all view
@@ -40,26 +62,17 @@ exports.fruit_view_all_Page = async function(req, res) {
     res.send(`{"error": ${err}}`);
     }
     };
-/*exports.fruit_list = async function(req, res) {
-        try{
-        thefruits = await fruit.find();
-        res.render('fruit', { title: 'fruit Search Results', results: thefruits });
-        }
-        catch(err){
-        res.status(500);
-        res.send(`{"error": ${err}}`);
-        }
-        };*/
 
 
-        // Handle Costume create on POST.
+
+        // Handle fruit create on POST.
 exports.fruit_create_post = async function(req, res) {
     console.log(req.body)
     let document = new fruit();
     // We are looking for a body, since POST does not have query parameters.
     // Even though bodies can be in many different formats, we will be picky
     // and require that it be a json object
-    // {"costume_type":"goat", "cost":12, "size":"large"}
+    // {"fruit_type":"goat", "cost":12, "size":"large"}
     document.NAME = req.body.NAME;
     document.CALORIES = req.body.CALORIES;
     document.COLOR = req.body.COLOR;
@@ -72,4 +85,48 @@ exports.fruit_create_post = async function(req, res) {
     res.send(`{"error": ${err}}`);
     }
     };
+
+    // Handle fruit delete on DELETE.
+exports.fruit_delete = async function(req, res) {
+    console.log("delete " + req.params.id)
+    try {
+    result = await fruit.findByIdAndDelete( req.params.id)
+    console.log("Removed " + result)
+    res.send(result)
+    } catch (err) {
+    res.status(500)
+    res.send(`{"error": Error deleting ${err}}`);
+    }
+    };
+
+    // Handle a show one view with id specified by query
+exports.fruit_view_one_Page = async function(req, res) {
+    console.log("single view for id " + req.query.id)
+    try{
+    result = await fruit.findById( req.query.id)
+    res.render('fruitdetail',
+    { title: 'fruit Detail', toShow: result });
+    }
+    catch(err){
+    res.status(500)
+    res.send(`{'error': '${err}'}`);
+    }
+    };
+
+
+
+    // Handle building the view for creating a fruit.
+// No body, no in path parameter, no query.
+// Does not need to be async
+exports.fruit_create_Page = function(req, res) {
+    console.log("create view")
+    try{
+    res.render('fruitcreate', { title: 'fruit Create'});
+    }
+    catch(err){
+    res.status(500)
+    res.send(`{'error': '${err}'}`);
+    }
+    };
+    
     
--- routes/fruit.js
+++ routes/fruit.js
@@ -3,4 +3,6 @@ const fruit_controlers= require('../controllers/fruit');
 var router = express.Router();
 /* GET fruits */
 router.get('/', fruit_controlers.fruit_view_all_Page );
+router.get('/detail', fruit_controlers.fruit_view_one_Page);
+router.get('/create', fruit_controlers.fruit_create_Page);
 module.exports = router;
